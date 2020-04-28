b827fccc96ed59af37617bd734f0e833ea100c41
victoria-gray <56879103+victoria-gray@users.noreply.github.com>
2020-04-28 14:12:43 +0100

    Add files via upload

--- get_dog.py
+++ get_dog.py
@@ -1,10 +1,10 @@
 import requests
 from pprint import pprint
 
-breed_search = input("What breed are you looking for?")
+# breed_search = input("What breed are you looking for?")
 
 api_key='3539d314-545c-4f60-a573-619dfa38bebb'
-base_url = 'https://dog.ceo/api/'.format(api_key)
+base_url = 'https://dog.ceo/api/'
 
 def _get(resource):
     """Sends a GET request to the provided resource and returns the 'message' data if it exists."""
@@ -13,20 +13,24 @@ def _get(resource):
     if res.status_code == 200:
         return res.json()['message']
     else:
-        return res
-    return data['message']
+        raise Exception("Invalid breed selected.")
 
 def master_breeds():
     """Gets all breeds, not including sub-breeds. Returns a list of breed names."""
     return _get('breeds/list')
 
+
 def random_image(breed_search):
     """Gets a random dog image. Returns a url as a string"""
-    if breed_search == random:
+    if breed_search == 'random':
         return _get('breeds/image/random')
     else:
         return _get('breed/{0}/images/random'.format(breed_search))
 
 
+if __name__ =='__main__':
+    print(master_breeds())
+    breed_search = input("Select a breed from the above list or write random.")
+    print(random_image(breed_search))
 
 
